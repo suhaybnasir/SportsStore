f8e0cc0e767366171fe7508a99beb35ac0625332
Suhayb Nasir <zubeirnasir@outlook.com>
2020-09-05 14:08:00 +0300

    Added repositories

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -31,6 +31,7 @@ namespace SportsStore
                 opts.UseSqlServer(
                     Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
